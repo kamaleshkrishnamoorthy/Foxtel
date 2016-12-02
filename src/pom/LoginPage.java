d400fcaa686ac6453671d572cb9c1ad5099cb97a
kamaleshkrishnamoorthy <kamaleshmk@gmail.com>
2016-12-02 20:41:38 +0530

    -

--- src/pom/LoginPage.java
+++ src/pom/LoginPage.java
@@ -1,13 +1,50 @@
 package pom;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class LoginPage extends BasePage {
 
+	@FindBy(id = "username")
+	private WebElement unTextBox;
+	// the above is a varaible untext of type webelement. only decalred
+	//will be initialized during runtime only.
+	
+	@FindBy(name = "pwd")
+	private WebElement pwTextBox;
+	
+	@FindBy(id = "LoginButton")
+	private WebElement loginButton;
+	
+	@FindBy(xpath = "/span[contains(.,'invalid')]")
+	private WebElement errmsg;
+	
 	public LoginPage(WebDriver driver) {
 		super(driver);
 		PageFactory.initElements(driver, this);
+	}
+	
+	public void SetUserName(String un)
+	{
+		unTextBox.sendKeys(un);
+	}
+	
+	public void SetPassword(String pw)
+	{
+		pwTextBox.sendKeys(pw);
+	}
+	
+	public void clickLogin()
+	{
+		loginButton.click();
+	}
+	
+	public void verifyErrMsgDisplayed()
+	{
+		Assert.assertTrue(errmsg.isDisplayed());
 	}
 	
 }
